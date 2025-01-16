9060e528dea20a01a29b3756a50cf4a2846b184c
balqisse-messaya <balqisse.messaya@laplateforme.io>
2025-01-16 11:25:38 +0100

    addition of a result operation feature

--- index.py
+++ index.py
@@ -1,70 +1,134 @@
+import os
+
 def operation_type():
+   v= input("operateur: ")
+   return v
 
-    """
-    Cette fonction demande à l'opérateur de choisir une fonction valide.
-    Retourne l'opération choisi sous forme de chaîne.
-    """
-    while True:
-        operation = input("Entrez le type d'operation souhaitée(+, -, *, /,).")
-        if operation in ["+", "-", "*", "/"]:
-            return operation 
+def check_int(x):    
+        if x %2 == 0:
+            int(x)
+        elif x %3 == 0:
+            int(x)
+        elif x %5 == 0:
+            int(x)
+        elif x %7 == 0:
+            int(x)
         else:
-            print (" Erreur. Operation invalide. Veuillez choisir parmi (+,-,*,/).")
+            float(x)
+                
 
-def resultat_operation (nombre1, nombre2, operation):
-    """
-    Cette fonction effectue l'opération demandée entres deux nombres
-    :param nombre1: Premier nombre
-    :param nombre2: Second nombre
-    :param operation: Type d'operation ( +, -, *, /)
-    :return: Le résultats de l'opération 
-    """
-    try:
-        if operation == "+":
-            return nombre1 + nombre2
-        elif operation == "-":
-            return nombre1 - nombre2
-        elif operation == "*":
-            return nombre1 * nombre2
-        elif operation == "/":
-            if nombre2 == 0:
-                raise ValueError("Erreur. Division par zéro")
-            return nombre1 / nombre2
-    except ValueError as e:
-        print(e)
-        return None
+def math_expression(): #commentaire qyui ser a rien
+    try: 
+        os.system('cls' if os.name == 'nt' else 'clear')
+        num_a = float(input("\nChoisissez le premier nombre de votre operation : "))
+        v= operation_type()
+        num_b = float(input("Choisissez le deuxième nombre de votre operation : "))
+        print()
+        
+        if (type(num_a) == float or type(num_a)== int) and (type(num_b) == float or type(num_b)== int): # or use 'isinstance'
+            check_int(num_a)
+            check_int(num_b)# marche pas a chequer 
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"Opération demandée: {num_a} {v} {num_b} \n")
+            print("résultat: \n")
+               
+            if v == "+":
+                print(f"                      {num_a + num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            elif v == "-":
+                print(f"                      {num_a - num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            elif v == "*":
+                print(f"                      {num_a * num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            elif v == "/":
+                print(f"                      {num_a / num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            elif v == "//":
+                print(f"                      {num_a // num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            elif v == "**":
+                cloud = print(f"                      {num_a ** num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            elif v == "%":
+                print(f"                      {num_a % num_b}")
+                print()
+                while True:
+                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    if reload == "q" or reload =="Q":
+                        break
+                    else:
+                        if reload != "":
+                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                        else:
+                            math_expression()
+            else:
+                print("Résultat impossible, opérateur non conforme.")
+                math_expression()
+        else: 
+            print("Vous pouvez seulement choisir des nombres décimaux ou entiers")
+            math_expression()
+    except ValueError:
+        print ("Erreur: saisie impossible pour opération. Recommencez")
+        math_expression()
+        
+        
+math_expression()
 
-def demander_nombre(message):
-    """
-    Cette fonction demande a l'utilisateur un nombre valide
-    :param message: Message à affciher
-    :return: Un nombre (float)
-    """
-    while True:
-        try:
-            return float(input(message))
-        except ValueError:
-            print("Erreur. Entrez un nombre valide")
 
-def main():
-    print("Bienvenue dans la calculatrice.")
+# def history():
 
-    nombre1 = demander_nombre("Entrez le premier nombre.")
-    nombre2 = demander_nombre("Entre le deuxième nombre.")
-
-    operation = operation_type()
-    
-    résultat= resultat_operation ( nombre1, nombre2, operation)
-    if résultat is not None:
-        print(f"Resultat  : {résultat}")
-
-if __name__== "__main__":
-    main()
-
-
-
-
-
-
-
- 
