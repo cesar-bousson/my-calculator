47a0431d271d30b7c72642331bd21248d7b8dbdd
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-01-16 11:59:48 +0100

    Code translation in English.

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 
 def operation_type():
-   v= input("operateur: ")
+   v= input("operator: ")
    return v
 
 def check_int(x):    
@@ -17,118 +17,114 @@ def check_int(x):
             float(x)
                 
 
-def math_expression(): #commentaire qyui ser a rien
+def math_expression():
     try: 
         os.system('cls' if os.name == 'nt' else 'clear')
-        num_a = float(input("\nChoisissez le premier nombre de votre operation : "))
+        num_a = float(input("\nChoose first number to operate : "))
         v= operation_type()
-        num_b = float(input("Choisissez le deuxième nombre de votre operation : "))
+        num_b = float(input("Choose second number : "))
         print()
         
         if (type(num_a) == float or type(num_a)== int) and (type(num_b) == float or type(num_b)== int): # or use 'isinstance'
             check_int(num_a)
-            check_int(num_b)# marche pas a chequer 
+            check_int(num_b) #Don't work (check) 
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"Opération demandée: {num_a} {v} {num_b} \n")
-            print("résultat: \n")
+            print(f"Asked operation : {num_a} {v} {num_b} \n")
+            print("result : \n")
                
             if v == "+":
                 print(f"                      {num_a + num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             elif v == "-":
                 print(f"                      {num_a - num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             elif v == "*":
                 print(f"                      {num_a * num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             elif v == "/":
                 print(f"                      {num_a / num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             elif v == "//":
                 print(f"                      {num_a // num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             elif v == "**":
                 cloud = print(f"                      {num_a ** num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             elif v == "%":
                 print(f"                      {num_a % num_b}")
                 print()
                 while True:
-                    reload = input("Appuyez sur Entrée pour relancer ou tapez Q pour quitter:")
+                    reload = input("Press enter to reload or press Q to quit:")
                     if reload == "q" or reload =="Q":
                         break
                     else:
                         if reload != "":
-                            print("Saisie incorrecte choisissez Entrée pour continuer ou Q pour arreter.")
+                            print("Incorrect entry. Press Enter to continue or Q to stop.")
                         else:
                             math_expression()
             else:
-                print("Résultat impossible, opérateur non conforme.")
+                print("Impossible result, non compliant operator.")
                 math_expression()
         else: 
-            print("Vous pouvez seulement choisir des nombres décimaux ou entiers")
+            print("You can only choose des float or integers.")
             math_expression()
     except ValueError:
-        print ("Erreur: saisie impossible pour opération. Recommencez")
+        print ("Error : Invalid syntax for operation. Retry lad.")
         math_expression()
         
         
 math_expression()
-
-
-# def history():
-
