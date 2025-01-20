aa3a29136a5df05174ba699dd87629b8aa1cdc8e
cesar-bousson <cesar.bousson@laplateforme.io>
2025-01-20 11:03:46 +0100

    checkout for soutenance

--- index.py
+++ index.py
@@ -5,14 +5,14 @@ import os
 def error_management(a,b,operator):
     if operator == "/" and b == 0 or a == 0:
         print("!!!!!! Error: division by zero impossible !!!! xO \n\n")
-        stop_relaunch()
+        stop_relaunch_erase()
     elif operator == "%" and b == 0 or a == 0 :
         print("!!!!!!! Error: modulo by zero impossible !!!! xO \n\n")
-        stop_relaunch()
+        stop_relaunch_erase()
         #raise ZeroDivisionError (I know this typeError message but i prefer print so i can relaunch the program)
     elif operator == "//" and b == 0 or a == 0:
         print("!!!!!!! Error: division by zero impossible !!!! xO \n\n")
-        stop_relaunch()
+        stop_relaunch_erase()
     else:
         return
     #or : if operator in ["/", "%", "//"] and b == 0:
@@ -84,14 +84,14 @@ def erase_historic(namefile = "historic_result.txt"):
     with open(namefile, "w") as file: # "w" write by erasing all
         file.write("") 
     
-# menu ----------------------------------------------------   
-def stop_relaunch():
+# end menu ----------------------------------------------------   
+def stop_relaunch_erase():
     while True:
         relaunch = input("     *_*_*_*_*_*_*_*_*_*_*_*_*_   Press [ENTER]: relaunch   |||   Press [Q] + enter: quit   |||   Press [E] + enter: erase historic *_*_*_*_*_*_*_*_*_*_*_*_*_  \n")
         if relaunch == "q" or relaunch =="Q":
             break
         elif relaunch == "e" or relaunch == "E":
-            erase_historic()
+            erase_historic() 
             print("historic erased")
             pass
         else:
@@ -109,9 +109,6 @@ def math_expression(): # main loop
         num_a = float(input("\nChoose first number to operate : "))
         operator = operation_type()
         num_b = float(input("Choose second number : "))
-                 
-        #verif int or float / no str permitted:       
-        # if (type(num_a) == float or type(num_a)== int) and (type(num_b) == float or type(num_b)== int):
             
         os.system('cls' if os.name == 'nt' else 'clear')
             
@@ -120,19 +117,16 @@ def math_expression(): # main loop
             
         result_h = result_expression(num_a, num_b, operator)
         print(f"{result_h}\n")
-        # result with 'int' return checking for return an int result if result is not a float.
          # optimized by function ! I saveeed so much liiiiiiines xO
              
-        write_historic(operation_h, check_int(result_h))    # Écrit dans l'historique.
+        write_historic(operation_h, check_int(result_h))    # write into historic
         print("historic : \n")
         print(f"{read_historic()} \n ")
 
-        stop_relaunch()                      
-        # else: 
-        #     print("You can only choose floats or integers.\n")
-        #     stop_relaunch()
+        stop_relaunch_erase()                      
+        
     except ValueError:
         print ("\n                                                     Error : Invalid syntax for operation. Please Retry.\n")
-        stop_relaunch()   
+        stop_relaunch_erase()   
 math_expression()
 
